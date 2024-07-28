d0be79cc8e2afb2c99546174c52e64c8b229dec3
brightalot <gamja9151@hanyang.ac.kr>
2024-07-28 17:52:54 +0900

    [feat][update user model][brightalot]

--- users/models.py
+++ users/models.py
@@ -1,5 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-class User(AbstractUser):
-    name = models.CharField(max_length=10)
+class UserManager(BaseUserManager):
+    def create_user(self, nickname, password=None, **extra_fields):
+        if not nickname:
+            raise ValueError('The Nickname field must be set')
+        user = self.model(nickname=nickname, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, nickname, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(nickname, password, **extra_fields)
+
+class User(AbstractBaseUser):
+    nickname = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=128)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'nickname'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.nickname
