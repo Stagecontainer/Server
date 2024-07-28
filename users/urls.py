b09bee73a68f302fef00c1d6d97c1b159e156773
brightalot <gamja9151@hanyang.ac.kr>
2024-07-28 17:27:10 +0900

    [feat][Implement user registeration feature][brightalot]

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import UserCreateView
 
 urlpatterns = [
-    path("", views.practice),
+    path('signup/', UserCreateView.as_view(), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import UserSerializer
+from .models import User
 
-def practice(request):
-    return HttpResponse("성공!!")
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
