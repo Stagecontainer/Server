b99bc385d396b87085509dabfe901904700eaf10
brightalot <gamja9151@hanyang.ac.kr>
2024-08-04 16:33:01 +0900

    solve merge comflicts

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import UserCreateView, LoginView
+from .views import UserCreateView, LoginView, CurrentUserView, GetUserIdView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('signup/', UserCreateView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('current_user/', CurrentUserView.as_view(), name='current_user'),  # 현재 사용자 정보 엔드포인트 추가
+    path('get_user_id/', GetUserIdView.as_view(), name='get_user_id'),  # 사용자 ID 조회 엔드포인트 추가
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.exceptions import ValidationError
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer, MyTokenObtainPairSerializer
 from .models import User
 
@@ -34,3 +36,36 @@ class UserCreateView(generics.CreateAPIView):
 
 class LoginView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+            data = serializer.validated_data
+            return Response(data, status=status.HTTP_200_OK)
+        except ValidationError as e:
+            return Response({
+                "errors": serializer.errors,
+                "message": "로그인에 실패했습니다."
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+class CurrentUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        return Response({'nickname': user.nickname}),status.HTTP_200_OK
+
+class GetUserIdView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        nickname = request.query_params.get('nickname')
+        if not nickname:
+            return Response({"error": "사용자 닉네임이 필요합니다."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            user = User.objects.get(nickname=nickname)
+            return Response({'id': user.id}), status.HTTP_200_OK
+        except User.DoesNotExist:
+            return Response({"error": "사용자를 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
