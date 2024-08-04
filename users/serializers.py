3f79331ac7738abb27bc4c27deac60b26457d167
brightalot <gamja9151@hanyang.ac.kr>
2024-08-04 16:44:56 +0900

    userserializer 복구

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,19 @@
+from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'nickname', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            nickname=validated_data['nickname'],
+            password=validated_data['password']
+        )
+        return user
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
